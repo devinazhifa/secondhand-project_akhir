c7bbfe405e30dd3a7713d6efb6c822cd2ca735b3
nvlhnn <hanannaufal17@gmail.com>
2022-07-04 20:20:16 +0700

    socket navbar

--- src/Components/Navbar/SecondaryNavbar.js
+++ src/Components/Navbar/SecondaryNavbar.js
@@ -1,11 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./SecondaryNavbar.module.css";
+import { io } from "socket.io-client";
 
 const SecondaryNavbar = () => {
+  const [notifs, setNotifs] = useState(null);
+
+  useEffect(() => {
+    const getNotif = async () => {
+      const res = await axios.get("http://localhost:3000/api/notifications", {
+        headers: {
+          Authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJ1c2VyQGdtYWlsLmNvbSIsImlhdCI6MTY1Njg0MDUyNn0.YY44QA_YZ2gmpYZjBZEyVHzAhkgqSntP1lP6SyXulEE",
+        },
+      });
+
+      setNotifs(res.data.data);
+    };
+
+    getNotif();
+
+    const socket = io("http://localhost:3000");
+
+    socket.on("connect", () => console.log(socket.id));
+
+    socket.on("notif", (data) => {
+      console.log("ada notif", data);
+      getNotif();
+    });
+  }, []);
+
+  console.log(notifs?.map((a) => a.id));
+
   return (
     <header className={style.header}>
-      <div className="container">
+      <div className={container}>
         <div className="row justify-content-between align-items-center">
           <div className="col-auto ml-3">
             <img
@@ -34,21 +64,29 @@ const SecondaryNavbar = () => {
                   type="submit"
                   className={`${style["icons-menu"]} btn d-flex align-items-center`}
                 >
-                  <i class="fa-solid fa-bars"></i>
+                  <i className="fa-solid fa-bars"></i>
                 </Link>
                 <Link
                   to="/"
                   type="submit"
                   className={`${style["icons-menu"]} btn d-flex align-items-center`}
                 >
-                  <i class="fa-regular fa-bell"></i>
+                  <i
+                    className="fa-regular fa-bell"
+                    style={{
+                      color: notifs?.some((a) => a.isRead === false)
+                        ? "red"
+                        : "black",
+                    }}
+                    data-count="12"
+                  ></i>
                 </Link>
                 <Link
                   to="/info-akun"
                   type="submit"
                   className={`${style["icons-menu"]} btn d-flex align-items-center`}
                 >
-                  <i class="fa-regular fa-user"></i>
+                  <i className="fa-regular fa-user"></i>
                 </Link>
               </li>
             </ul>
